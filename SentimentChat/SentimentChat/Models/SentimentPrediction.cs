a4cf55e5e746409a285051a073dcb99d133afbc6
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-03-30 03:03:16 +0500

    Add SentimentPrediction Model

---
+++ SentimentChat/SentimentChat/Models/SentimentPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+namespace SentimentChat.Models
+{
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabel { get; set; } = string.Empty;
+        public float[] Score { get; set; } = Array.Empty<float>();
+    }
+}
