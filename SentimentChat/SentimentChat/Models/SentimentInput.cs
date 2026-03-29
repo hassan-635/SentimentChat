f786a2779b3fe8c718bcc47e08b8bc8f4dd2fd8f
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-03-30 03:00:43 +0500

    Add SentimentInput Model

---
+++ SentimentChat/SentimentChat/Models/SentimentInput.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Data;
+namespace SentimentChat.Models
+{
+    public class SentimentInput
+    {
+        [LoadColumn(0)]
+        public string Text { get; set; } = string.Empty;
+        [LoadColumn(1)]
+        [ColumnName("Label")]
+        public string Label { get; set; } = string.Empty;
+    }
+}
